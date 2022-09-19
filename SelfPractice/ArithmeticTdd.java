791206c0e9f80f970f73112fd87949366518383e
Ernest Emmanuel Utibe <110993733+morsecodes007@users.noreply.github.com>
2022-09-19 09:24:43 +0100

    Add files via upload

--- SelfPractice/ArithmeticTdd.java
+++ SelfPractice/ArithmeticTdd.java
@@ -3,18 +3,10 @@ package SelfPractice;
 public class ArithmeticTdd {
     public  int  number;
 
-//    public ArithmeticTdd(){
-//        this.number = num;
-//    }
     public int getSquareOfNumbers(int num) {
         return num * num;
     }
     public int getDifferenceOfNumbers(int num1, int num2){
-//        if (num2 < num1){
-//            int temp = num2;
-//            num2 = num1;
-//            num1 = temp;
-//        }
         return Math.abs(num2 - num1);
     }
 
